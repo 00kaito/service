91b023799e159d860fae8b5180eb1ac549ff37c5
dell <bednarz@programmer.net>
2024-08-25 22:58:56 -0400

    test for cycle controller + load utils for test data

---
+++ src/test/java/com/bednarz/usmobile/LoadDataUtil.java
@@ -0,0 +1,38 @@
+package com.bednarz.usmobile;
+
+import com.bednarz.usmobile.domain.cycle.Cycle;
+import com.bednarz.usmobile.domain.dailyusage.DailyUsage;
+import com.bednarz.usmobile.domain.user.User;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class LoadDataUtil {
+
+    public static List<User> loadUsers(ObjectMapper objectMapper, String filePath) throws IOException {
+        InputStream inputStream = new ClassPathResource(filePath).getInputStream();
+        return objectMapper.readValue(inputStream, new TypeReference<List<User>>() {
+        });
+    }
+
+    public static List<Cycle> loadCycles(ObjectMapper objectMapper, String filePath) throws IOException {
+        InputStream inputStream = new ClassPathResource(filePath).getInputStream();
+        return objectMapper.readValue(inputStream, new TypeReference<List<Cycle>>() {
+        });
+    }
+
+    public static List<DailyUsage> loadDailyUsage(ObjectMapper objectMapper, String filePath) throws IOException {
+        InputStream inputStream = new ClassPathResource(filePath).getInputStream();
+        return objectMapper.readValue(inputStream, new TypeReference<List<DailyUsage>>() {
+        });
+    }
+
+    public static <T> void insertData(List<T> data, MongoTemplate mongoTemplate) {
+        mongoTemplate.insertAll(data);
+    }
+}
--- src/test/java/com/bednarz/usmobile/api/CycleControllerTest.java
+++ src/test/java/com/bednarz/usmobile/api/CycleControllerTest.java
@@ -1,19 +1,13 @@
 package com.bednarz.usmobile.api;
 
 import com.bednarz.usmobile.BaseTest;
-import com.bednarz.usmobile.MongodbContainer;
+import com.bednarz.usmobile.domain.cycle.Cycle;
 import com.bednarz.usmobile.domain.cycle.CycleRepository;
-import com.bednarz.usmobile.domain.dto.CreateUserRequest;
+import com.bednarz.usmobile.domain.dailyusage.DailyUsage;
 import com.bednarz.usmobile.domain.dto.CycleDataResponse;
-import com.bednarz.usmobile.domain.dto.UpdateUserRequest;
+import com.bednarz.usmobile.domain.user.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.JsonPath;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,25 +16,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.utility.MountableFile;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
+import static com.bednarz.usmobile.LoadDataUtil.*;
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-class CycleControllerTest extends MongodbContainer {
+class CycleControllerTest extends BaseTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -51,15 +42,40 @@ class CycleControllerTest extends MongodbContainer {
     @Autowired
     CycleRepository cycleRepository;
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+
     @BeforeEach
-    public void cleanUp(){
-//        cycleRepository.deleteAll();
+    void setUp() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<User> users = loadUsers(objectMapper, "test-data/cycle-controller-test/users.json");
+        List<Cycle> cycles = loadCycles(objectMapper, "test-data/cycle-controller-test/cycles.json");
+        List<DailyUsage> dataUsage = loadDailyUsage(objectMapper, "test-data/cycle-controller-test/dailyUsage.json");
+
+        insertData(users, mongoTemplate);
+        insertData(cycles, mongoTemplate);
+        insertData(dataUsage, mongoTemplate);
+
+        /*        DBObject objectToSave = BasicDBObjectBuilder.start()
+                .add("key", "value")
+                .get();
+        mongoTemplate.findAll(DBObject.class, "collection");
+*/
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        mongoTemplate.dropCollection(User.class);
+        mongoTemplate.dropCollection(DailyUsage.class);
+        mongoTemplate.dropCollection(Cycle.class);
     }
 
     @Test
     public void createCycleTest() throws Exception {
         CycleDataResponse request = new CycleDataResponse();
-        request.setMdn("9999999999");
+        request.setMdn("1234567890");
         request.setStartDate(new Date());
         request.setEndDate(new Date());
 
@@ -67,7 +83,7 @@ class CycleControllerTest extends MongodbContainer {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.data.mdn").value("0000000000"));
+                .andExpect(jsonPath("$.data.mdn").value("1234567890"));
     }
 
     @Test
@@ -81,14 +97,12 @@ class CycleControllerTest extends MongodbContainer {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error", containsString("MDN must not be blank")));
+                .andExpect(jsonPath("$.error", containsString("MDN must be 10 characters length")));
     }
 
     @Test
     public void getCycleHistoryTest() throws Exception {
-        String mdn = "9999999999";
-
-        MongoTemplate template = MongoTemplateZ
+        String mdn = "2345678901";
 
         ResultActions resultActions = mockMvc.perform(get("/api/v1/cycles/history")
                         .param("mdn", mdn))
@@ -105,13 +119,13 @@ class CycleControllerTest extends MongodbContainer {
         ResultActions resultActions = mockMvc.perform(get("/api/v1/cycles/history")
                         .param("mdn", mdn))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error", containsString("MDN must not be blank")));
+                .andExpect(jsonPath("$.error", containsString("MDN must be 10 characters length")));
     }
 
     @Test
     public void getCurrentCycleUsageTest() throws Exception {
-        String mdn = "1234567890";
-        String userId = "user123";
+        String mdn = "3456789012";
+        String userId = "test-user-id-3";
 
         ResultActions resultActions = mockMvc.perform(get("/api/v1/cycles/usage/current")
                         .param("mdn", mdn)
@@ -123,12 +137,12 @@ class CycleControllerTest extends MongodbContainer {
     @Test
     public void getCurrentCycleUsageTest_ReturnsValidationErrors() throws Exception {
         String mdn = "";  // Invalid MDN
-        String userId = "user123";
+        String userId = "test-user-id-3";
 
         ResultActions resultActions = mockMvc.perform(get("/api/v1/cycles/usage/current")
                         .param("mdn", mdn)
                         .param("userId", userId))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error", containsString("MDN must not be blank")));
+                .andExpect(jsonPath("$.error", containsString("MDN must be 10 characters length")));
     }
 }
